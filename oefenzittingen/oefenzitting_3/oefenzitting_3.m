64879485aac8fbdf033249008d17a323688b9758
Ward Schodts <wardschodts@hotmail.com>
2014-05-24 03:47:20 +0200

    oefenzitting_3

--- oefenzittingen/oefenzitting_3/oefenzitting_3.m
+++ oefenzittingen/oefenzitting_3/oefenzitting_3.m
@@ -3,3 +3,18 @@ b = bepaalb()
 p = bepaalp(b)
 
 %% Probleem 2
+
+dumpfp(0.125);
+dumpfp(0.25);
+dumpfp(0.5);
+dumpfp(1);
+dumpfp(2);
+dumpfp(4);
+dumpfp(8);
+dumpfp(0.001);
+dumpfp(0.01);
+dumpfp(0.1);
+dumpfp(1);
+dumpfp(10);
+dumpfp(100);
+dumpfp(1000);
